b91e7bec1792b725c3079c90785b8541021dff23
shubham07nand@gmail.com <91931931+shubhamnand@users.noreply.github.com>
2022-02-26 13:55:08 +0530

    hello

---
+++ add.java
@@ -0,0 +1,11 @@
+//program for addition of 2 numbers
+
+class add
+{
+	public static void main(String s[])
+	{
+		int a=10,b=20,sum;
+		sum=a+b;
+		System.out.println(sum);
+	}
+}
---
+++ calc.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class calc()
+{
+
+      public static void main(String args[]) 
+      {
+            int a,b;
+            a= 20;
+            b=10;
+            
+            int c = a+b;
+            System.out.println(c);
+      }
+
+}
